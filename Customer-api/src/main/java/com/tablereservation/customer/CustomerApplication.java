99c5fd85e9bed9da0d30b808ab728e42ccea2cbe
yongsuKim <yongsu0312.kim@gmail.com>
2023-04-08 02:10:29 +0900

    Initial commit

---
+++ Customer-api/src/main/java/com/tablereservation/customer/CustomerApplication.java
@@ -0,0 +1,22 @@
+package com.tablereservation.customer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableFeignClients
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
+@RequiredArgsConstructor
+@ServletComponentScan
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+
+}
