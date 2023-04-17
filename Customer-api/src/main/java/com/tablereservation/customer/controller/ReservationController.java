9e98e4251c3b116dee9e2faec627d873e3184a41
yongsuKim <yongsu0312.kim@gmail.com>
2023-04-17 13:16:49 +0900

    [009-CUSTOMER-Reservation_Search] 예약 정보 조회 기능 구현
    Comments
    - Reservation_Id로 에약 정보 조회 기능 구현
    - test코드, scratch 작성

--- Customer-api/src/main/java/com/tablereservation/customer/controller/ReservationController.java
+++ Customer-api/src/main/java/com/tablereservation/customer/controller/ReservationController.java
@@ -2,6 +2,7 @@ package com.tablereservation.customer.controller;
 
 import com.tablereservation.customer.application.ReservationApplication;
 import com.tablereservation.customer.domain.ReservationForm;
+import com.tablereservation.customer.domain.model.Reservation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,5 +17,11 @@ public class ReservationController {
     public ResponseEntity<String> createReservation(@RequestBody ReservationForm form) {
         Long reservationId = reservationApplication.createReservation(form);
         return ResponseEntity.ok("예약이 성공적으로 생성되었습니다. 예약 ID: " + reservationId);
+    }
+
+    @GetMapping("/{reservationId}")
+    public ResponseEntity<Reservation> getReservation(@PathVariable Long reservationId) {
+        Reservation reservation = reservationApplication.getReservationById(reservationId);
+        return ResponseEntity.ok(reservation);
     }
 }
--- Customer-api/src/test/java/com/tablereservation/customer/application/ReservationApplicationTest.java
+++ Customer-api/src/test/java/com/tablereservation/customer/application/ReservationApplicationTest.java
@@ -4,6 +4,7 @@ import com.tablereservation.customer.domain.ReservationForm;
 import com.tablereservation.customer.domain.model.Reservation;
 import com.tablereservation.customer.service.ReservationService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,6 +31,7 @@ public class ReservationApplicationTest {
     }
 
     @Test
+    @DisplayName("예약 생성 Test")
     public void createReservationTest() {
         ReservationForm form = ReservationForm.builder()
                 .store_id(1L)
@@ -48,5 +50,31 @@ public class ReservationApplicationTest {
         assertThat(reservationId).isEqualTo(1L);
 
         verify(reservationService, times(1)).createReservation(form);
+    }
+
+    @Test
+    @DisplayName("예약 조회 Test")
+    public void getReservationByIdTest() {
+        Long reservationId = 1L;
+        Reservation reservation = Reservation.builder()
+                .reservation_id(reservationId)
+                .store_id(1L)
+                .customer_phone("010-1234-5678")
+                .reservation_time(LocalDateTime.now().plusDays(1))
+                .reservation_status("예약됨")
+                .build();
+
+        when(reservationService.getReservationById(reservationId)).thenReturn(reservation);
+
+        Reservation foundReservation = reservationApplication.getReservationById(reservationId);
+
+        assertThat(foundReservation).isEqualTo(reservation);
+        assertThat(foundReservation.getReservation_id()).isEqualTo(reservationId);
+        assertThat(foundReservation.getStore_id()).isEqualTo(reservation.getStore_id());
+        assertThat(foundReservation.getCustomer_phone()).isEqualTo(reservation.getCustomer_phone());
+        assertThat(foundReservation.getReservation_time()).isEqualTo(reservation.getReservation_time());
+        assertThat(foundReservation.getReservation_status()).isEqualTo(reservation.getReservation_status());
+
+        verify(reservationService, times(1)).getReservationById(reservationId);
     }
 }
